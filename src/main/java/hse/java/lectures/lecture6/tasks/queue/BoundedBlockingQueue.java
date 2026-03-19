238a40f1f1a3401ade9ed979ac155986fe66bbab
illidvn <panoffilya@gmail.com>
2026-03-19 23:42:13 +0300

    queue: write one more blocking queue in my life

--- src/main/java/hse/java/lectures/lecture6/tasks/queue/BoundedBlockingQueue.java
+++ src/main/java/hse/java/lectures/lecture6/tasks/queue/BoundedBlockingQueue.java
@@ -1,25 +1,52 @@
 package hse.java.lectures.lecture6.tasks.queue;
 
-public class BoundedBlockingQueue<T> {
+import java.util.ArrayDeque;
+import java.util.Deque;
 
+public class BoundedBlockingQueue<T> {
+    private final Deque<T> queue;
+    private final int capacity;
 
     public BoundedBlockingQueue(int capacity) {
-
+        if (capacity <= 0) {
+            throw new IllegalArgumentException("Capacity must be > 0");
+        }
+        this.queue = new ArrayDeque<>();
+        this.capacity = capacity;
     }
 
     public void put(T item) throws InterruptedException {
+        if (item == null) {
+            throw new IllegalArgumentException("Item must be non null");
+        }
 
+        synchronized (queue) {
+            while (queue.size() == capacity) {
+                queue.wait();
+            }
+            queue.addLast(item);
+            queue.notifyAll();
+        }
     }
 
     public T take() throws InterruptedException {
-        return null;
+        synchronized (queue) {
+            while (queue.isEmpty()) {
+                queue.wait();
+            }
+            T item = queue.removeFirst();
+            queue.notifyAll();
+            return item;
+        }
     }
 
     public int size() {
-        return 0;
+        synchronized (queue) {
+            return queue.size();
+        }
     }
 
     public int capacity() {
-        return 0;
+        return this.capacity;
     }
 }
